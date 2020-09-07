de1ce5be73374e1c82d25b7735e76800258aa9bb
Cristian Bravo Lillo <cristian.bravo@gmail.com>
2020-09-07 18:02:47 -0300

    Small additions to analysis.r

--- data-analysis/analysis.r
+++ data-analysis/analysis.r
@@ -1,10 +1,17 @@
+#> We load the data
 results <- read.csv('./results.csv')
 
 #> We create convenient names for the variables that are interesting
+results$age <- results$What.is.your.age.
+results$gender <- results$What.is.your.gender....Selected.Choice
 results$total <- results$How.many.passwords.in.total.does.your.Password.Manager.manage.for.you.
 results$weak <- results$How.many.weak.passwords.in.total.does.your.Password.Manager.report.
 results$duplicate <- results$How.many.duplicate.reused.repeated.non.unique.passwords.in.total.does.your.Password.Manager.report.
 results$method <- results$When.you.are.creating.an.account.on.a.website.or.changing.your.password..are.you.more.likely.to...Selected.Choice
+
+#> Let's check demographics
+summary(results$What.is.your.age.)
+table(results$What.is.your.gender....Selected.Choice)
 
 #> We examine normality for variables (not expecting it... just taking a look)
 hist(results$total)
@@ -20,7 +27,8 @@ kruskal.test(duplicate ~ method, data = results)
 #> We remove those folks with less than 10 passwords
 results <- results[ -c(results$total<10), ]
 
-#> Let's check whether distributions for both methods are different
+#> Let's check again whether distributions for both methods are different
 kruskal.test(total ~ method, data = results)
 kruskal.test(weak ~ method, data = results)
 kruskal.test(duplicate ~ method, data = results)
+
