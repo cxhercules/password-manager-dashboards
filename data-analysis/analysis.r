5717eeee0c40625fa8bde5afd34fe08862849331
Cristian Bravo Lillo <cristian.bravo@gmail.com>
2021-03-25 17:01:24 -0300

    Adding data and script

--- data-analysis/analysis.r
+++ data-analysis/analysis.r
@@ -1,9 +1,35 @@
-#> We load the data
-results <- read.csv('./results.csv')
+
+#> We load and clean the data
+thesedata <- read.csv('./data/Pilot-20210315.csv')
+thesedata$gender <- factor(thesedata$X5.3)
+thesedata$age <- thesedata$X5.2
+
+for (i in c('Status','Progress','Finished','DistributionChannel','UserLanguage','ResponseId','RandomNumber',
+			'SESSION_ID','StudyID','ResponseID','Referer','SurveyID','X5.3','X5.2','Q_RecaptchaScore'))
+	thesedata[,c(i)] <- NULL
+rm(i)
+
+#> Compacting specific columns
+thesedata$Q.WhichPwdManager <- factor(thesedata$Q.WhichPwdManager)
+thesedata$Q.WhichPwdManager.other <- NA
+thesedata$Q.WhichPwdManager.other[thesedata$Q.WhichPwdManager=='Another Browser\'s Built-In Password Manager (please type the name below)'] <-
+	paste(thesedata$Q.WhichPwdManager_17_TEXT[thesedata$Q.WhichPwdManager_17_TEXT!=''])
+thesedata$Q.WhichPwdManager[thesedata$Q.WhichPwdManager=='Another Browser\'s Built-In Password Manager (please type the name below)'] <- as.factor("another.browser")
+
+thesedata$Q.WhichPwdManager.other[thesedata$Q.WhichPwdManager=='Other Password Manager (please type the name below)'] <-
+	paste(thesedata$Q.WhichPwdManager_18_TEXT[thesedata$Q.WhichPwdManager_18_TEXT!=''])
+thesedata$Q.WhichPwdManager[thesedata$Q.WhichPwdManager=='Other Password Manager (please type the name below)'] <- "another.pwdmanager"
+
+thesedata$Q.WhichPwdManager_17_TEXT <- NULL
+thesedata$Q.WhichPwdManager_18_TEXT <- NULL
+
+
+#> We count how many people gave their consent
+thesedata$Q.Duration <- paste(thesedata$Q.Duration)
+
+
 
 #> We create convenient names for the variables that are interesting
-results$age <- results$What.is.your.age.
-results$gender <- results$What.is.your.gender....Selected.Choice
 results$total <- results$How.many.passwords.in.total.does.your.Password.Manager.manage.for.you.
 results$weak <- results$How.many.weak.passwords.in.total.does.your.Password.Manager.report.
 results$duplicate <- results$How.many.duplicate.reused.repeated.non.unique.passwords.in.total.does.your.Password.Manager.report.
